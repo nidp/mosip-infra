2d65ee05903645f3c4ae5e1ef36ffaf14f17aa2a
Baakal Tesfaye <baakal@gmail.com>
2020-09-23 15:09:08 +0300

    replace kc1 alias cmd on monitoring script

--- deployment/sandbox-v2/monitoring-off.sh
+++ deployment/sandbox-v2/monitoring-off.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Run this script to exchange turn off pods in monitoring namespace
-kc1 scale deployment elasticsearch-master --replicas=0
-kc1 scale deployment graf-grafana  --replicas=0
-kc1 scale deployment kibana-kibana --replicas=0
-kc1 scale deployment metrics-server --replicas=0
-kc1 scale deployment prom-prometheus-alertmanager --replicas=0
-kc1 scale deployment prom-prometheus-pushgateway --replicas=0
-kc1 scale deployment prom-prometheus-server --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment elasticsearch-master --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment graf-grafana  --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment kibana-kibana --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment metrics-server --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-alertmanager --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-pushgateway --replicas=0
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-server --replicas=0
--- deployment/sandbox-v2/monitoring-on.sh
+++ deployment/sandbox-v2/monitoring-on.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Run this script to exchange turn off pods in monitoring namespace
-kc1 scale deployment elasticsearch-master --replicas=1
-kc1 scale deployment graf-grafana  --replicas=1
-kc1 scale deployment kibana-kibana --replicas=1
-kc1 scale deployment metrics-server --replicas=1
-kc1 scale deployment prom-prometheus-alertmanager --replicas=1
-kc1 scale deployment prom-prometheus-pushgateway --replicas=1
-kc1 scale deployment prom-prometheus-server --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment elasticsearch-master --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment graf-grafana  --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment kibana-kibana --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment metrics-server --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-alertmanager --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-pushgateway --replicas=1
+kubectl --kubeconfig $HOME/.kube/mzcluster.config scale deployment prom-prometheus-server --replicas=1
